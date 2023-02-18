7ad3164b91a415afffb14e04fd71f20b59908597
Vadosdavos <bezbvap@gmail.com>
2023-02-18 12:41:07 +0300

    fixed canvas on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from 'react';
 import { Background } from './components/background/background';
 import { Main } from './components/main/main';
 
 function App() {
+  const [isBgVisible, setSsBgVisible] = useState(false);
+  useEffect(() => {
+    const brackepoint = window.matchMedia('(max-width: 1200px)');
+    if (brackepoint.matches) {
+      setSsBgVisible(false);
+    } else {
+      setSsBgVisible(true);
+    }
+  }, []);
   return (
     <>
-      <Background />
+      {isBgVisible && <Background />}
       <Main />
     </>
   );
